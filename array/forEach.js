043d0a2a31aa73da97fba5a74409326e5fa70f67
Pearl Yeh <yeh326797@gmail.com>
2021-02-17 22:37:09 +0800

    forEach function

---
+++ array/forEach.js
@@ -0,0 +1,74 @@
+var data = [
+  {
+    mid: 785541,
+    cid: 61,
+    count: 1,
+    date: "",
+    favorite: false,
+    name: "印度班加罗尔超级联赛",
+    priority: 190088,
+    sid: 1,
+    tid: 8511,
+  }, {
+    mid: 885644,
+    cid: 0,
+    count: 5,
+    date: "",
+    favorite: false,
+    name: "柬埔寨总理杯",
+    priority: 190116,
+    sid: 1,
+    tid: 11185,
+  }, {
+    mid: 546023,
+    cid: 61,
+    count: 1,
+    date: "",
+    favorite: false,
+    name: "印度沙希德曼诺兰锦标赛",
+    priority: 195942,
+    sid: 1,
+    tid: 20722,
+  },
+]
+
+var midVaule = data[i].mid
+var midData={
+  midVaule:{}
+}
+for (var i=0; i<data.length;i++){
+  console.log(midVaule)
+}
+
+
+
+// 最後輸出：
+// {
+//   [mid] : { },
+//   [mid] : { },
+//   [mid] : { },
+// }
+
+// example：
+var output = {
+  785541: {
+    mid: 785541,
+    cid: 61,
+    count: 1,
+    date: "",
+    favorite: false,
+    name: "印度班加罗尔超级联赛",
+    priority: 190088,
+    sid: 1,
+    tid: 8511,
+  },
+  885644: {},
+  546023: {},
+}
+
+var newData={}
+data.forEach(function (item, index) {
+  newData[data[index].mid] = item;
+})
+
+console.log(newData)
